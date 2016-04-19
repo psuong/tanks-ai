d0d4b2d59ec15e4e236f0be7fdb84370d5aaf565
Porrith Suong <psuong95@gmail.com>
2016-04-19 10:32:25 -0400

    Adds public fields to the player controls.

---
+++ Assets/Scripts/Player Control/Movement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Movement : MonoBehaviour {
+
+    // Public fields
+    public string horizontalInput = "Horizontal";
+    public string verticalInput = "Vertical";
+    public float playerSpeed = 5f;
+    public float turnSpeed = 30f;
+    
+    // Private fields
+
+	// Use this for initialization
+	private void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+	
+	}
+
+    private void FixedUpdate() {
+
+    }
+}
