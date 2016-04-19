43635973dfa8232e0e7d97aceb0a0751d8c8ad81
Porrith Suong <psuong95@gmail.com>
2016-04-19 10:12:50 -0400

    Adds a bool trigger in the animator.

--- Assets/Scripts/AI/Conditions/AI_Conditions.cs
+++ Assets/Scripts/AI/Conditions/AI_Conditions.cs
@@ -6,14 +6,24 @@ public class AI_Conditions : MonoBehaviour {
     public GameObject player;
     public float minimalDistance = 10f;
     public float sightAngle = 120;
+    
+    // The following strings below are meant to be hashed such that Unity
+    // can find the IDs within the state machine.
+    public string withinSightName = "See Enemy";
+
+    // Some private fields.
     private Animator stateMachine;
+    private int withinSightID;
 
 	// Use this for initialization
 	private void Start () {
+        // Get the animator componenet attached to the AI controlled tank.
         stateMachine = GetComponent<Animator>();
         if (player == null) {
             player = GameObject.FindWithTag("Player");
         }
+
+        withinSightID = Animator.StringToHash(withinSightName);
 	}
 	
 	// Update is called once per frame
@@ -21,6 +31,7 @@ public class AI_Conditions : MonoBehaviour {
         // If the AI is within distance and within an angle of the player, then switch states.
         if (IsWithinSightDistance() && IsWithinPeripherals()) {
             // TODO: Set a trigger that forces the state machine to change states.
+            stateMachine.SetBool(withinSightName, true);
         }
 	}
     
