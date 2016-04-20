1794584a3ab7f50c4d3ec4afd6ae59f34222dcda
Porrith Suong <psuong95@gmail.com>
2016-04-20 13:33:21 -0400

    Adds a rudimetrary player fire control.

--- Assets/Scripts/Player Control/Attack.cs
+++ Assets/Scripts/Player Control/Attack.cs
@@ -2,18 +2,30 @@
 using System.Collections;
 
 public class Attack : MonoBehaviour {
-
-    public GameObject projectile;
+    
+    public Rigidbody projectile;
+    public Transform shotPosition;
 
     public string fireName = "Fire1";
 
+    public float shotForce = 1000f;
+    public float fireRate = 0.5f;
+
+    private float nextFireInterval;
+    
 	// Use this for initialization
 	private void Start () {
-	    
+        nextFireInterval = 0f;   
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	private void Update () {
+	    if (Input.GetKeyUp(fireName) && Time.time > nextFireInterval) {
+            nextFireInterval = Time.time + fireRate;
+            
+            // Create an instance of the shot and move it forward.
+            Rigidbody shot = Instantiate(projectile, shotPosition.position, Quaternion.identity) as Rigidbody;
+            shot.AddForce(shotPosition.forward * shotForce);
+        }
 	}
 }
