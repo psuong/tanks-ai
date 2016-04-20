b4793fca51f4212378042d8a052cf2b55bd1b96c
Porrith Suong <psuong95@gmail.com>
2016-04-20 17:03:39 -0400

    Adds AI_Attack implementation to create a projectile.

--- Assets/Scripts/AI/Actions/AI_Attack.cs
+++ Assets/Scripts/AI/Actions/AI_Attack.cs
@@ -6,22 +6,37 @@ public class AI_Attack : StateMachineBehaviour {
 
     public GameObject player;
     public Rigidbody projectile;
+    public Transform shotPosition;
+
+    public float shotForce = 1000f;
+    public float shotInterval = 0.5f;
+
+    private float nextShotInterval;
 
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        // If there is no instance of the player, let's access the AI_Conditions component and 
+        // assign a reference for this state.
         if (player == null) {
             player = animator.gameObject.GetComponent<AI_Conditions>().player;
         }
+        
+        // Let's set the interval when the state has been accessed.
+        nextShotInterval = 0f;
 
-        // TODO: Rotate towards the player and let's fire a projectile.
+        // Rotate towards the player and let's fire a projectile.
+        animator.transform.LookAt(player.transform);
+
+        Shoot();
     }
 
-    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex, AnimatorControllerPlayable controller) {
-        base.OnStateEnter(animator, stateInfo, layerIndex, controller);
-    }
-    
-    // The Shoot() method fires off a projectile.
+    // The Shoot() method creates a projectile and launches it forward.
     private void Shoot() {
+        if (Time.time > nextShotInterval) {
+            nextShotInterval = Time.time + shotInterval;
+            Rigidbody shot = Instantiate(projectile, shotPosition.position, Quaternion.identity) as Rigidbody;
 
+            shot.AddForce(shotPosition.forward * shotForce);
+        }
     }
     
 }
