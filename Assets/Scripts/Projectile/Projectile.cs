8e744a4f15248d791333dddfe6aad9c62e0c7611
Porrith Suong <psuong95@gmail.com>
2016-04-21 16:48:50 -0400

    Adds a projectile script.

---
+++ Assets/Scripts/Projectile/Projectile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+    public float damage = 20f;
+    
+    public void OnTriggerEnter(Collider other) {
+        // WARNING: Don't use the SendMessage function. It's pretty slow
+        // compared to a direct call or using C# events and delegates.
+        // But...I used it because I'm pretty lazy. :D
+        if (other.CompareTag("Player") || other.CompareTag("Enemy")) {
+            other.SendMessage("TakeDamage", damage);
+        }
+    }
+}
